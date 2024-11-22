bdf2fe128f255e73392b071b448843ec2536f9b5
Norm Brandinger <n.brandinger@gmail.com>
2024-11-21 22:32:23 -0500

    Minor updates

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,10 +1,29 @@
 advent_of_code::solution!(2);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut total = 0;
+    for line in input.lines() {
+        // take line in the format of lxwxh and split it into a vector of u32
+        let numbers: Vec<u32> = line
+            .split('x')
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+
+        let s1 = numbers[0] * numbers[1];
+        let s2 = numbers[1] * numbers[2];
+        let s3 = numbers[2] * numbers[0];
+
+        // find the minimum of the three sides
+        let min_side = s1.min(s2).min(s3);
+
+        // calculate the total area of the box
+        total += 2 * s1 + 2 * s2 + 2 * s3 + min_side;
+    }
+    println!("Total: {}", total);
+    Some(total)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 
@@ -15,7 +34,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(58));
     }
 
     #[test]
